8a68dc794bba49c35f1d902593f183e62eb0fb68
zhoulin <497311172@qq.com>
2023-06-30 13:56:27 +0800

    修改前缀

---
+++ BGZLDB/dictToModelController/dictToModelController.h
@@ -0,0 +1,13 @@
+//
+//  dictToModelController.h
+//  BGZLDB
+//
+//  Created by huangzhibiao on 17/3/14.
+//  Copyright © 2017年 Biao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dictToModelController : UIViewController
+
+@end
